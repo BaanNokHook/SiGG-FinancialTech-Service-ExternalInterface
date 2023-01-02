7982d26c8dd3e2194f4ea9da1f718ae50b723e30
SiGGFedCJKP <baannokhooksg@gmail.com>
2023-01-02 16:47:06 +0700

    SiGG-FinancialTech-Service-ExternalInterface-Quater-HomeController

---
+++ HomeController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GM.Service.ExternalInterface.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index() => "Repo External Interface API";
+    }
+}
